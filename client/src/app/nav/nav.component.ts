66641be37f164a302d641283447611144cc6cd7d
Arno Maat <arnomaat@valkdegroot.nl>
2025-02-06 13:34:04 +0100

    half of section 5

---
+++ client/src/app/nav/nav.component.ts
@@ -0,0 +1,33 @@
+import * as core from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AccountsService } from '../_services/accounts.service';
+import { inject} from '@angular/core';
+
+
+@core.Component({
+  selector: 'app-nav',
+  imports: [FormsModule],
+  templateUrl: './nav.component.html',
+  styleUrl: './nav.component.css'
+})
+export class NavComponent {
+  private accountService = inject(AccountsService);
+  loggedIn = false;
+  model: any ={};
+
+  login() {
+    this.accountService.login(this.model).subscribe({
+      next: response => {
+        console.log(response);
+        this.loggedIn = true; 
+      },
+      error: error => console.log(error)
+
+    })
+  } 
+
+  logout() {
+    this.loggedIn = false;
+  }
+
+}
