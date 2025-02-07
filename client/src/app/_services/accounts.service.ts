af897935a39cfb22e36e48f973ba792c6b7a1b94
Pandemo77 <arno.maat@gmail.com>
2025-02-07 16:01:47 +0100

    End of section 5

--- client/src/app/_services/accounts.service.ts
+++ client/src/app/_services/accounts.service.ts
@@ -1,14 +1,44 @@
 import { HttpClient } from '@angular/common/http';
-import { Injectable, inject, model } from '@angular/core';
+import { Injectable, inject, model, signal } from '@angular/core';
+import { User } from '../_Models/user';
+import { map } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
-export class AccountsService {
+export class AccountService {
   private http = inject(HttpClient);
-  baseUrl = 'https://localhost:7088/api/'
+  baseUrl = 'https://localhost:7088/api/';
+  currentUser = signal< User | null>(null);
 
   login(model: any){
-    return this.http.post(this.baseUrl + 'account/login', model);
+    return this.http.post<User>(this.baseUrl + 'account/login', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+      }
+      )
+    )
   }
+
+  register(model: any){
+    return this.http.post<User>(this.baseUrl + 'account/register', model).pipe(
+      map(user => {
+        if (user) {
+          localStorage.setItem('user', JSON.stringify(user));
+          this.currentUser.set(user);
+        }
+        return user;
+      }
+      )
+    )
+  }
+
+  logout() {
+    localStorage.removeItem('user');
+    this.currentUser.set(null);
+  }
+
 }
--- client/src/app/nav/nav.component.ts
+++ client/src/app/nav/nav.component.ts
@@ -1,25 +1,25 @@
 import * as core from '@angular/core';
 import { FormsModule } from '@angular/forms';
-import { AccountsService } from '../_services/accounts.service';
+import { AccountService } from '../_services/accounts.service';
 import { inject} from '@angular/core';
+import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 
 
 @core.Component({
   selector: 'app-nav',
-  imports: [FormsModule],
+  imports: [FormsModule, BsDropdownModule],
   templateUrl: './nav.component.html',
   styleUrl: './nav.component.css'
 })
 export class NavComponent {
-  private accountService = inject(AccountsService);
-  loggedIn = false;
+  accountService = inject(AccountService);
   model: any ={};
 
   login() {
     this.accountService.login(this.model).subscribe({
       next: response => {
         console.log(response);
-        this.loggedIn = true; 
+       
       },
       error: error => console.log(error)
 
@@ -27,7 +27,7 @@ export class NavComponent {
   } 
 
   logout() {
-    this.loggedIn = false;
+    this.accountService.logout();
   }
 
 }
